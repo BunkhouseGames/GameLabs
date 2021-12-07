81d54bd0b3ca9b7b92155fc0b3cb2098f46e38c3
Gisli Konradsson <gisli@arctictheory.com>
2021-12-07 16:19:04 +0000

    Add AT gameplay modules to the cookiecutter initializer

--- add-project.bat
+++ add-project.bat
@@ -1,2 +1,2 @@
 cookiecutter cookiecutter-templates\TemplateProject -o _generated_projects/
-explorer .\_generated_projects\
+rem explorer .\_generated_projects\
--- cookiecutter-templates/TemplateProject/hooks/post_gen_project.py
+++ cookiecutter-templates/TemplateProject/hooks/post_gen_project.py
@@ -1,11 +1,24 @@
 import subprocess
 import os
 import shutil
-import time
+
 
 subprocess.call("git init")
+
+# Clone myself into the target project
 subprocess.call(["git", "submodule", "add", "git@github.com:arctictheory/GameLabs.git"])
 
-# os.chdir("..")
+# Setup AT Game labs
+core_plugins = [
+    ["git@github.com:arctictheory/kamo.git", "Kamo"],
+    ["git@github.com:arctictheory/RedisClient.git", "RedisClient"],
+    ["git@github.com:arctictheory/ATTools.git", "ATTools"],
+    ["git@github.com:arctictheory/SideQuest.git", "SideQuest"],
+    ["git@github.com:arctictheory/ATCommon.git", "ATCommon"],
+]
+
+for url, name in core_plugins:
+    cmd = ["git", "submodule", "add", url, f"Plugins/ATCorePlugins/{name}"]
+    subprocess.run(cmd)
 
 print(f"New project generated at: {os.getcwd()}")
