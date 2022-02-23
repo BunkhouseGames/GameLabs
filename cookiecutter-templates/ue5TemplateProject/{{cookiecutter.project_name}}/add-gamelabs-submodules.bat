276c4f2cc4a0d25cf03990842744640758ef72ab
Gisli Konradsson <gisli@arctictheory.com>
2022-02-23 15:49:35 +0000

    Add file to inject the gamelabs modules

---
+++ cookiecutter-templates/ue5TemplateProject/{{cookiecutter.project_name}}/add-gamelabs-submodules.bat
@@ -0,0 +1,8 @@
+git submodule add git@github.com:arctictheory/SyntyContentData.git Content/ContentPacks/Synty
+git submodule add git@github.com:arctictheory/kamo.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/Kamo
+git submodule add git@github.com:arctictheory/RedisClient.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/RedisClient
+git submodule add git@github.com:arctictheory/ATTools.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/ATTools
+git submodule add git@github.com:arctictheory/SideQuest.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/SideQuest
+git submodule add git@github.com:arctictheory/ATCommon.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/ATCommon
+git submodule add git@github.com:arctictheory/LandmassEngine.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/GenOS
+git submodule add git@github.com:arctictheory/ATUpVote.git {{cookiecutter.project_name}}Game/Plugins/ATCorePlugins/ATUpVote
