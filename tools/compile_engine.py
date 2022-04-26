5750869704a7705884d1198eecf675a73e227883
Gisli Konradsson <gisli@arctictheory.com>
2022-04-26 10:55:12 +0000

    add compile tools

--- tools/compile_engine.py
+++ tools/compile_engine.py
@@ -1,23 +1,111 @@
 import sys
 import pathlib
-import zipfile
+import copy
 import subprocess
 
 engine_root = sys.argv[1]
 project_file_path = sys.argv[2]
 
+engine = pathlib.Path(engine_root)
+project = pathlib.Path(project_file_path)
+
 
 def compile_engine(engine_root_path, project_path):
-    subprocess.run([engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"), "Win64", "Development", "ShaderCompileWorker"])
-    subprocess.run([engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"), "Win64", "Development", "UnrealPak"])
-    subprocess.run([engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"), "Win64", "Development", "UnrealPak", "D:/Work/ArcticTheory/ProjectDawn/ProjectDawnGame/ProjectDawnPreview.uproject", "-TargetType=Editor"])
+    subprocess.run(
+        [
+            engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"),
+            "Win64",
+            "Development",
+            "ShaderCompileWorker",
+        ]
+    )
+    subprocess.run(
+        [
+            engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"),
+            "Win64",
+            "Development",
+            "UnrealPak",
+        ]
+    )
+    subprocess.run(
+        [
+            engine_root_path.joinpath("Engine", "Build", "BatchFiles", "RunUBT.bat"),
+            "Win64",
+            "Development",
+            "UnrealPak",
+            "D:/Work/ArcticTheory/ProjectDawn/ProjectDawnGame/ProjectDawnPreview.uproject",
+            "-TargetType=Editor",
+        ]
+    )
 
     pass
 
-engine  = pathlib.Path(engine_root)
-project = pathlib.Path(project_file_path)
 
-print(engine)
-print(project)
+build_flags = {
+    "existence_windows_debug_client": [
+        "-clientconfig=Debug",
+        "-cook",
+        "-iterate",
+        "-nop4",
+        "-nocompileeditor",
+        "-build",
+        "-stage",
+        "-archive",
+        "-pak",
+        "-nodebuginfo",
+        "-noPCH",
+        "-prereqs",
+        "-CrashReporter",
+    ],
+    "existence_windows_shipping_client": [
+        "-clientconfig=Development",
+        "-cook",
+        "-iterate",
+        "-nop4",
+        "-nocompileeditor",
+        "-build",
+        "-stage",
+        "-archive",
+        "-pak",
+        "-nodebuginfo",
+        "-noPCH",
+        "-prereqs",
+        "-CrashReporter",
+    ],
+    "kamo_linux_server": [
+        "-serverconfig=Development",
+        "-targetplatform=Linux",
+        "-cook",
+        "-server",
+        "-nocompileeditor",
+        "-build",
+        "-stage",
+        "-archive",
+        "-pak",
+        "-NoClient",
+        "-package",
+        "-nodebuginfo",
+        "-targetplatform=Linux",
+        "-prereqs",
+    ],
+}
 
-compile_engine(engine, project)
+
+def build_game(engine, project):
+    run_uat = f"{engine}/Engine/Build/BatchFiles/RunUAT.bat"
+    cmd = [
+        run_uat,
+        "BuildCookRun",
+        f"-project={project}",
+        # f"-archivedirectory={output_folder}",
+    ]
+
+    flags = copy.copy(build_flags["existence_windows_shipping_client"])
+    cmd.extend(flags)
+
+    subprocess.run(cmd)
+
+
+# compile_engine(engine, project)
+
+build_game(engine, project)
