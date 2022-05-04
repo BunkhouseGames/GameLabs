b02524e03fc7f5744a93f6df54bf244c00d2ba0d
Gisli Konradsson <gisli@arctictheory.com>
2022-05-04 15:27:46 +0000

    Move submodule logic into the commandline

--- cookiecutter-templates/ue5TemplateProject/hooks/post_gen_project.py
+++ cookiecutter-templates/ue5TemplateProject/hooks/post_gen_project.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 subprocess.run(["git", "init"])
-subprocess.call(["git", "submodule", "add", "git@github.com:arctictheory/GameLabs.git"])
+subprocess.call(["add-gamelabs-submodules.bat"])
 
 subprocess.call(["git", "submodule", "update", "--init"])
 print(f"New project generated at: {os.getcwd()}")
