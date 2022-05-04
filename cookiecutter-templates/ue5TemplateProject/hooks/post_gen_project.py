69b55155f8fa952d1770036bf69fb484d19bf8e6
Gisli Konradsson <gisli@arctictheory.com>
2022-05-04 10:09:09 +0000

    Fix the synty content installer and fix the add project names

--- cookiecutter-templates/ue5TemplateProject/hooks/post_gen_project.py
+++ cookiecutter-templates/ue5TemplateProject/hooks/post_gen_project.py
@@ -4,4 +4,5 @@ import os
 subprocess.run(["git", "init"])
 subprocess.call(["git", "submodule", "add", "git@github.com:arctictheory/GameLabs.git"])
 
+subprocess.call(["git", "submodule", "update", "--init"])
 print(f"New project generated at: {os.getcwd()}")
--- cookiecutter-templates/ue5TemplateProject/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}Game/add-synty-content.bat
+++ cookiecutter-templates/ue5TemplateProject/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}Game/add-synty-content.bat
@@ -1,1 +1,2 @@
-git submodule add git@github.com:arctictheory/SyntyContentData.git Content/ContentPacks/Synty --depth 1
+cd ..
+git submodule add git@github.com:arctictheory/SyntyContentData.git {{cookiecutter.project_name}}Game/Content/ContentPacks/Synty
