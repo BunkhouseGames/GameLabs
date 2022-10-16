2d5a6b88d896a0585512830d1c53f4775faabc38
Gisli Konradsson <gislikonradsson@gmail.com>
2022-10-16 09:12:46 +0000

    Compile the editor after the template has been created

--- templates/ue5/hooks/post_gen_project.py
+++ templates/ue5/hooks/post_gen_project.py
@@ -15,13 +15,11 @@ def _get_project_name():
     uproject_file = list(new_project_root.glob("**/*.uproject"))[0]
     return uproject_file.stem
 
-
 name = _get_project_name()
 
 shutil.copytree(
     gamelabs_root.joinpath("extras", "tools"), new_project_root.joinpath("tools")
 )
-
 
 target_path = new_project_root.joinpath(_get_project_name(), "Plugins")
 os.makedirs(str(target_path))
@@ -31,3 +29,16 @@ shutil.copytree(
     gamelabs_root.joinpath("extras", "ExistenceSDK"),
     new_project_root.joinpath(target_path, "ExistenceSDK"),
 )
+
+build_command = ["python", "tools/build_game2.py", "run-build-action", "--profile", "compile"]
+unreal_engine_map_command = ["python", "tools/map_engine_to_repo.py", "map-from-launcher"]
+
+print("Running cmd:")
+print(unreal_engine_map_command)
+
+subprocess.call(unreal_engine_map_command)
+
+print("Running cmd:")
+print(build_command)
+
+subprocess.call(build_command)
