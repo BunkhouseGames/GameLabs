43d7794e13a12eeba01a68528f60d5bdbe855805
Gisli Konradsson <gislikonradsson@gmail.com>
2022-10-16 07:48:03 +0000

    Remove support for UE4

--- templates/TemplateProject/{{cookiecutter.project_name}}/Source/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.cpp
+++
@@ -1,6 +0,0 @@
-// {{cookiecutter.copyright}}
-
-#include "{{cookiecutter.project_name}}.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, {{cookiecutter.project_name}}, "{{cookiecutter.project_name}}" );
